e483eb7bbec7a639ed407c80aa189ef700d9ef28
ZhengShenli <2041297003@qq.com>
2022-10-11 20:18:39 +0800

    第二次修改

--- demo-double-1/src/com/zheng/Hello.java
+++ demo-double-1/src/com/zheng/Hello.java
@@ -3,8 +3,5 @@ package com.zheng;
 public class Hello {
     public static void main(String[] args) {
         System.out.println(12);
-        System.out.println(12);
-        System.out.println(12);
-        System.out.println(17);
     }
 }
