34e9cc62e302bf4f52bc7c7ad5a5a88c0525c09d
ZhengShenli <2041297003@qq.com>
2022-10-12 20:53:44 +0800

    zheng修改了，郑身利喜欢古风的

--- demo/src/com/zheng/Hello.java
+++ demo/src/com/zheng/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("郑身利大帅哥11。");
         System.out.println("郑身利喜欢年轻的美女。");
         System.out.println("郑身利喜欢成熟的美女");
+        System.out.println("郑身利喜欢古风美女");
     }
 }
